e602af33bbcaa491235d5f6fed900ae364cbfd93
KAGamerYT <51527445+KAGamerYT@users.noreply.github.com>
2025-05-11 03:49:30 +0300

    EuroSAT app.py

--- EuroSAT app.py
+++ EuroSAT app.py
@@ -5,7 +5,7 @@ from PIL import Image
 import numpy as np
 
 # Load model
-model = load_model("EuroSAT Model.h5")
+model = load_model("my_model.h5")
 
 # Define classes
 class_names = ['AnnualCrop', 'Forest', 'HerbaceousVegetation', 'Highway', 'Industrial', 
