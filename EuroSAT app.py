f360d5c576d9d88bf7889027298dd5cccdddfe32
KAGamerYT <51527445+KAGamerYT@users.noreply.github.com>
2025-05-11 03:57:15 +0300

    EuroSAT app.py

--- EuroSAT app.py
+++ EuroSAT app.py
@@ -28,6 +28,8 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
+    st.write("📷 Image Shape:", img_array.shape)
+    st.write("✅ Model Input Shape:", model.input_shape)
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Preprocess and predict
